b3b8551e253ea5a77cf31050426d333597dc2550
Sam-Orion <121442461+Sam-Orion@users.noreply.github.com>
2025-03-28 02:23:22 +0530

    Replaced token logic with jwt

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ app.use(express.json());
 
 const users = [];
 
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// function generateToken() {
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
+//     let token = "";
+//     for (let i = 0; i < 32; i++) {
+//         // use a simple function here
+//         token += options[Math.floor(Math.random() * options.length)];
+//     }
+//     return token;
+// }
+
+const JWT_SECRET = "USER_APP";
 
 app.post("/signup", function(req, res) {
     const username = req.body.username;
@@ -42,10 +44,12 @@ app.post("/signin", function(req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    const user = users.find(user => user.username === username && user.password === password);
+    let user = users.find(user => user.username === username && user.password === password);
 
     if (user) {
-        const token = generateToken();
+        const token = jwt.sign({
+            username: user.username
+        }, JWT_SECRET);
         user.token = token;
         res.send({
             token
@@ -61,7 +65,9 @@ app.post("/signin", function(req, res) {
 
 app.get("/me", (req, res) => {
     const token = req.headers.authorization;
-    const user = users.find(user => user.token === token);
+    const userDetail = jwt.verify(token, JWT_SECRET);
+    const username = userDetail.username;
+    const user = users.find(user => user.username === username);
     if (user) {
         res.send({
             username: user.username
